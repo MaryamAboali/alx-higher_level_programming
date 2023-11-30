ce737c66649389994a6a69153b902f04cbeabbf2
MaryamAboali <maryamaboali33@gmail.com>
2023-11-30 13:41:05 -0800

    a

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif count == 1:
         print("1 arguments.")
-    else
+    else:
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i +1, sys.argv[i + 1]))
